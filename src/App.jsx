165802ad088a5e19aa2ac37c3b121b3a254bbbf1
Thomas Portolano <tportolano@christies.com>
2024-06-03 15:22:32 +0800

    Cleaning up code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Card from "./components/card/Card";
 import Data from "./data.js";
 
-export default function App(props) {
+export default function App() {
   const cardArray = Data.map((card) => {
     return <Card key={card.id} card={card} />;
   });
