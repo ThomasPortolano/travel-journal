e6d453b2667c051bbf4fe9dd585ebd761f609e87
Thomas Portolano <tportolano@christies.com>
2024-06-03 15:17:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import Navbar from './components/navbar/Navbar'
-import Card from './components/card/Card'
-import Data from './data.js'
+import Navbar from "./components/navbar/Navbar";
+import Card from "./components/card/Card";
+import Data from "./data.js";
 
 export default function App(props) {
-  console.log(props)
-  
-
-  const cardArray = Data.map(card => {
-    return (
-      <Card
-      key = {card.id}
-      // location = {card.location}
-      // title = {card.title}
-      // startDate = {card.startDate}
-      // endDate = {card.endDate}
-      // description = {card.description}
-      // url = {card.googleMapsUrl}
-      // image = {card.imageUrl}
-      card = {card}
-      />
-    )
-    console.log(cardArray)
-  })
+  const cardArray = Data.map((card) => {
+    return <Card key={card.id} card={card} />;
+  });
 
   return (
     <div>
       <Navbar />
-      <div className="card-list">
-        {cardArray}
-      </div>
+      <div className="card-list">{cardArray}</div>
     </div>
-  )
+  );
 }
